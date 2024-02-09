4e72c24c9214277afa8f40e9c11ac7a66b081e4a
Alexandr Mun <mun_a2006@yahoo.de>
2024-02-09 13:40:31 +0600

    EJ-001 pattern builder

---
+++ src/kz/mun/se/effective_java/Pizza.java
@@ -0,0 +1,20 @@
+package kz.mun.se.effective_java;
+
+import java.util.EnumSet;
+import java.util.Objects;
+
+public abstract class Pizza {
+    public enum Topping {
+        HAM, MUSHROOM, ONION, PEPPER, SAUSAGE
+    }
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+        abstract Pizza build();
+        protected abstract T self();
+    }
+}
