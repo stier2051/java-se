2d545c008c25532689318d63f356cb9de3ad3df9
Alexandr Mun <mun_a2006@yahoo.de>
2024-02-15 15:09:16 +0600

    EF-004 some refactoring

--- src/kz/mun/se/effective_java/create_remove_objects/SingletonField.java
+++ src/kz/mun/se/effective_java/create_remove_objects/SingletonField.java
@@ -1,4 +1,4 @@
-package kz.mun.se.effective_java.creat_remove_objects;
+package kz.mun.se.effective_java.create_remove_objects;
 
 //singleton pattern - с полем public final
 public class SingletonField {
