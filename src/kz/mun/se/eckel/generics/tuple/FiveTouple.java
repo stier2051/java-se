5c4912e79003b7d2ef6b719f72e0398ef65e7fa2
Мун Александр Илларионович <a.mun@alataucitybank.kz>
2026-05-01 09:52:13 +0500

    [ECKEL-002] generics

---
+++ src/kz/mun/se/eckel/generics/tuple/FiveTouple.java
@@ -0,0 +1,14 @@
+package kz.mun.se.eckel.generics.tuple;
+
+public class FiveTouple<A, B, C, D, E> extends FourTuple<A, B, C, D> {
+    public final E fifth;
+
+    public FiveTouple(A a, B b, C c, D d, E e) {
+        super(a, b, c, d);
+        fifth = e;
+    }
+
+    public String toString() {
+        return "(" + first + ", " + second + ", " + third + ", " + fourth + ", " + fifth + ")";
+    }
+}
